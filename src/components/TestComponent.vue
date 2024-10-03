c9a14ee8c4a43b3bb5ca15a429ffa2db60972300
vitorCamarotto <vitorranacamarotto@gmail.com>
2024-10-03 17:04:07 -0300

    Add library configs

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
