b4d24f7678db52302360881056a34390f2301925
vitorCamarotto <vitorranacamarotto@gmail.com>
2024-10-04 09:17:43 -0300

    Update button behaviour

--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="increaseCountAndLog">count is {{ count }}</button>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 
+
+
 const count = ref(0)
+
+function increaseCountAndLog() {
+  count.value++
+
+  console.log('hey, you just clicked me')
+}
 </script>
